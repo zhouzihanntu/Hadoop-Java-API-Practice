9a3a229b4aa9436c3652b406d9882807e9f3235d
zhouzihanntu <abbey357423606@hotmail.com>
2016-05-14 16:37:07 +0800

    Create src.java

---
+++ src.java
@@ -0,0 +1,49 @@
+package hdfs;
+
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+public class java1 {
+	
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		mkDir(conf);
+		putData(conf);
+	}
+	
+	//Create Directory in hdfs
+	private static void mkDir(Configuration conf) throws Exception {
+		FileSystem fs=FileSystem.get(new URI("hdfs://192.168.102.132:9000/"), conf);
+		Path dir = new Path("/user/FileSystemTest");
+		boolean result = fs.mkdirs(dir);
+		System.out.println("make dir :" + result);
+		fs.close();
+	}
+	
+	//Upload file/directory to hdfs
+	private static void putData(Configuration conf) throws Exception{
+		Path src = new Path("/Users/zhouzihan/Downloads/test.txt");
+		FileSystem fs=FileSystem.get(new URI("hdfs://192.168.102.132:9000/"), conf);
+		Path dire = new Path("/user/FileSystemTest");
+		fs.copyFromLocalFile(src,dire);
+		System.out.println("put data :" + conf.get("fs.default.name"));
+		FileStatus files[] = fs.listStatus(dire);
+		for(FileStatus file:files){
+			System.out.println(file.getPath());
+		}
+	}
+	
+	//Download file/directory from hdfs
+	private static void getData(Configuration conf) throws Exception{
+		Path loc =new Path("/Users/zhouzihan/Desktop");
+		FileSystem fs=FileSystem.get(new URI("hdfs://192.168.102.132:9000/"), conf);
+		Path remote = new Path("/user/FileSystemTest/test.txt");
+		fs.copyToLocalFile(false, remote, loc);
+		System.out.println("copy data :" + conf.get("fs.default.name"));
+	}
+
+}
